7e0155720eb8e076102a9335d5a49fcc17b26343
KarstenPoddig <karsten_poddig@web.de>
2020-05-01 09:38:18 +0200

    added example for pca

--- pca_1.py
+++ pca_1.py
@@ -1,3 +1,20 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
+m = 10000
+n = 20
+
+X =  np.random.randn(m*n).reshape(n, m)
+B = np.random.exponential(size=n*n).reshape(n,n)
+X = np.dot(B, X)
+sigma = np.cov(X)
+sigma_theoretical = np.dot(B ,B.T)
+X = X.T
+
+
+from sklearn.decomposition import PCA
+
+pca = PCA()
+pca.fit(X)
+
+
